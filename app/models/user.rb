5b6cfad0a75001d2f7f18fa5e588dea1d0549bb1
costcon <tentekomambo@gmail.com>
2021-05-03 11:26:55 +0000

    Finish 4

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,9 +13,15 @@ class User < ApplicationRecord
   has_many :passive_relationships, class_name:"Relationship", foreign_key:"follower_id", dependent: :destroy
   has_many :followers, through: :passive_relationships, source: :followed
 
+  def followed_by?(user)
+    passive_relationships.find_by(followed_id: user.id).present?
+  end
 
   attachment :profile_image, destroy: false
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction, length: { maximum: 50 }
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users,only: [:show,:index,:edit,:update] do
     resource :relationships, only: [:create, :destroy]
-      get :follows, on: :menber
-      get :followers, on: :menber
+    get :follows, on: :member
+    get :followers, on: :member
+  end
+  
   resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
