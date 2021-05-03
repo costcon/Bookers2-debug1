c08d908b614bb8cd59e9b0e45dd2475acd83d041
costcon <tentekomambo@gmail.com>
2021-05-03 06:10:45 +0000

    Add

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users,only: [:show,:index,:edit,:update]
+    resource :relationships, only: [:create, :destroy]
+      get :follows, on: :menber
+      get :followers, on: :menber
   resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
