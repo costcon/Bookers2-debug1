7a763738ee6f3575728c25f89f608cdb112adcc3
costcon <tentekomambo@gmail.com>
2021-05-06 14:42:24 +0000

    Finish 7

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,18 @@ class User < ApplicationRecord
   has_many :passive_relationships, class_name:"Relationship", foreign_key:"follower_id", dependent: :destroy
   has_many :followers, through: :passive_relationships, source: :followed
 
+  include JpPrefecture
+  jp_prefecture :prefecture_code
+
+  def prefecture_name
+   JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
+  end
+
+  def prefecture_name=(prefecture_name)
+   self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
+  end
+
+
   def followed_by?(user)
     passive_relationships.find_by(followed_id: user.id).present?
   end
