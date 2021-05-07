4a86a618a1b4fd0b4a8f4501ace677e99965a8f9
costcon <tentekomambo@gmail.com>
2021-05-07 14:21:50 +0000

    Add 7

--- app/assets/javascripts/address_autofill.js
+++ app/assets/javascripts/address_autofill.js
@@ -1,10 +1,12 @@
-$(function() {
-  return $('#user_postcode').jpostal({
-    postcode: ['#user_postcode'],
-    address: {
-      '#user_prefecture_code': '%3',
-      '#user_address_city': '%4',
-      '#user_address_street': '%5%6%7',
-    },
+$(function () {
+  $(document).on('turbolinks:load', () => {
+    $('#user_postal_code').jpostal({
+      postcode: ['#user_postal_code'],
+      address: {
+        "#user_prefecture_code": "%3",
+        "#user_city": "%4%5",
+        "#user_street": "%6%7"
+      }
+    });
   });
 });
